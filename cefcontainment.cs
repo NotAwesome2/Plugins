d9e798bd9f258b361af77bc921462b1e6ef9890d
Goodlyay <zackxv7@yahoo.com>
2023-07-25 21:46:52 -0700

    Update cefcontainment.cs
    • /chat command, allows you to send to local chat without being in a local chat map
    • Prefixing message with $$ sends it to local chat
    • youtube and imgur are exclusively allowed in cef commands
    • cef click is disabled for security reasons
    • messages containing bad words aren't sent at all, ops informed

--- cefcontainment.cs
+++ cefcontainment.cs
@@ -3,6 +3,7 @@ using System.Threading;
 using System.Collections.Generic;
 using System.IO;
 using MCGalaxy;
+using MCGalaxy.Commands;
 using MCGalaxy.Events.ServerEvents;
 using MCGalaxy.Events.LevelEvents;
 using MCGalaxy.Events.PlayerEvents;
@@ -10,27 +11,165 @@ using MCGalaxy.Events.PlayerEvents;
 namespace MCGalaxy {
     
     public sealed class PluginCefContainment : Plugin {
+        
+        static string[] allowedWebsites = new string[] { "https://www.youtube.com/", "https://youtu.be/", "https://i.imgur.com" };
+        
         public override string name { get { return "CefContainment"; } }
-        public override string MCGalaxy_Version { get { return "1.9.0.5"; } }
+        public override string MCGalaxy_Version { get { return "1.9.4.9"; } }
         public override string creator { get { return "Goodly"; } }
         
         public override void Load(bool startup) {
-            OnPlayerChatEvent.Register(RightBeforeChat, Priority.High);
+            Command.Register(new CmdChat());
+            OnPlayerChatEvent.Register(OnPlayerChat, Priority.High);
+            OnPlayerCommandEvent.Register(OnPlayerCommand, Priority.Low);
         }
         
         public override void Unload(bool shutdown) {
-            OnPlayerChatEvent.Unregister(RightBeforeChat);
+            Command.Unregister(Command.Find("Chat"));
+            OnPlayerChatEvent.Unregister(OnPlayerChat);
+            OnPlayerCommandEvent.Unregister(OnPlayerCommand);
         }
         
-        static void RightBeforeChat(Player p, string message) {
+        const string LOCAL_PREFIX = "$$";
+        
+        static void OnPlayerChat(Player p, string message) {
+            bool local = message.StartsWith(LOCAL_PREFIX) || CmdChat.IsLocal(p);
             
-            if (Colors.Strip(message).StartsWith("cef ")) {
+            if (message.StartsWith(LOCAL_PREFIX)) {
+                message = message.Substring(LOCAL_PREFIX.Length);
+            }
+            
+            string stripped = Colors.Strip(message);
+            if (stripped.StartsWith("cef ")) {
+                if (!LevelInfo.Check(p, p.Rank, p.level, "use cef commands in this level")) { p.cancelchat = true; return; }
+                
+                string[] split = stripped.SplitSpaces(2);
+                if (split.Length < 2) { return; }
+                if (!AllowedCefCommand(p, split[1])) { p.cancelchat = true; return; }
+                
+                local = true;
+            }
+            
+            
+            string filteredMessage = message;
+            bool hasSlur = ProfanityFilter.Parse(filteredMessage) != message;
+            if (hasSlur) {
                 p.cancelchat = true;
-                if (!LevelInfo.Check(p, p.Rank, p.level, "use cef commands in this level.")) return;
-                Chat.MessageFromLevel(p, "<Local>λFULL: &f" +message);
+                if (message.CaselessContains("$skin") || message.CaselessContains("sskin")) {
+                    p.Message("&cThe &b$&bskin &ctoken is blocked from chat to avoid confusing messages.");
+                } else if (!message.CaselessEq("phag") && message.CaselessContains("phag") || message.CaselessContains("roofage")) {
+                    p.Message("&cOops! &SYour message has triggered a false positive in our slur filter. Try rewording it.");
+                } else {
+                    p.Message("&cThere's better ways to get attention kiddo. Read &b/rules");
+                }
+                
+                Chat.MessageOps("&STo Ops: "+p.name+" was blocked from saying \""+message+"\"");
+                Logger.Log(LogType.SuspiciousActivity, "CEFcontainment: {0} was blocked from saying \"{1}\"", p.name, message);
+            }
+            
+            
+            if (local) { SendAsLocal(p, message); }
+        }
+        static void SendAsLocal(Player p, string message) {
+            p.cancelchat = true;
+            Chat.MessageChat(ChatScope.Level, p, "<Local>λFULL: &f" + message, p.level, null);
+        }
+        
+        static bool HasCef(Player p) {
+            return p.Session.appName != null && p.Session.appName.Contains("+ cef");
+        }
+        static void OnPlayerCommand(Player p, string cmd, string args, CommandData data) {
+            if (!HasCef(p)) { return; }
+            if (cmd.CaselessEq("pclients")) {
+                //why would I do such a thing? to remove its usage from /last
+                Command.Find("pclients").Use(p, args);
+                p.cancelcommand = true;
+            }
+        }
+        
+        
+        
+        static string[] urlCommands = new string[] { "create", "play", "search", "queue" };
+        static bool AllowedCefCommand(Player p, string message) {
+            
+            string[] cefCommandArgs = message.SplitSpaces(2);
+            string cefCommand = cefCommandArgs[0];
+            if (cefCommand.CaselessEq("click")) { p.Message("&WSorry, cef click is disabled for security reasons."); return false; }
+            
+            if (cefCommandArgs.Length < 2) { return true; } //only one arg, okay to send since no url
+            string cefArgs = cefCommandArgs[1];
+            
+            bool isUrlCommand = false;
+            foreach (string command in urlCommands) {
+                if (command.CaselessEq(cefCommand)) { isUrlCommand = true; break; }
+            }
+            if (!isUrlCommand) { return true; } //no URL commands, okay to send
+            
+            
+            string[] args = cefArgs.SplitSpaces();
+            
+            string url = null;
+            foreach (string arg in args) {
+                if (arg[0] == '-') { continue; } //flag, ignore
+                url = arg;
+            }
+            if (url == null) { return true; } //no url, okay to send
+            
+            bool allowed = false;
+            foreach (string site in allowedWebsites) {
+                if (url.StartsWith(site)) { allowed = true; break; }
+            }
+            if (url.CaselessContains("redirect")) { allowed = false; }
+            if (!allowed) { p.Message("&WThe url {0} is not allowed in cef.", url); return false; }
+            
+            return true;
+        }
+    }
+    
+    public class CmdChat : Command2
+    {
+        public override string name { get { return "Chat"; } }
+        public override string shortcut { get { return ""; } }
+        public override bool MessageBlockRestricted { get { return true; } }
+        public override string type { get { return "other"; } }
+        public override bool museumUsable { get { return false; } }
+        public override LevelPermission defaultRank { get { return LevelPermission.Admin; } }
+        
+        const string KEY = "CmdChat_IsLocal";
+        
+        const string LOCAL_FLAG = "local", GLOBAL_FLAG = "global";
+        public override CommandAlias[] Aliases {
+            get { return new[] {
+                    new CommandAlias("LocalChat", LOCAL_FLAG), new CommandAlias("GlobalChat", GLOBAL_FLAG),
+                    new CommandAlias("lc", LOCAL_FLAG), new CommandAlias("gc", GLOBAL_FLAG)
+                }; }
+        }
+        
+        public override void Use(Player p, string message, CommandData data)
+        {
+            if (message.CaselessEq(LOCAL_FLAG)) {
+                if (IsLocal(p)) { p.Message("You are already in <Local> chat mode. Use &T/gc&S to turn it off."); return; }
+                p.Extras[KEY] = true; p.Message("<Local> chat mode (for you) is now &aON&S.");
                 return;
             }
-
+            if (message.CaselessEq(GLOBAL_FLAG)) {
+                if (!IsLocal(p)) { p.Message("You are already in normal chat mode. Use &T/lc&S to turn <Local> chat mode on."); return; }
+                p.Extras.Remove(KEY); p.Message("<Local> chat mode (for you) is now &cOFF&S.");
+                return;
+            }
+            p.Message("&cUse either &T/lc&c or &T/gc&c to turn on local or global chat mode.");
+        }
+        public static bool IsLocal(Player p) {
+            if (!p.Extras.Contains(KEY)) { return false; }
+            return (bool)p.Extras[KEY];
+        }
+        public override void Help(Player p)
+        {
+            p.Message("%T/LocalChat");
+            p.Message("%HMakes &byour&H messages send to &S<Local>&H chat.");
+            p.Message("To change the whole map to local chat, use /os map chat");
+            p.Message("%T/GlobalChat");
+            p.Message("%HUndoes the effects of %T/LocalChat");
         }
     }
 }
