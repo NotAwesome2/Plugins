4ec0406b3ab4b9cf1d28f127e6504a2017ce0459
Goodlyay <zackxv7@yahoo.com>
2023-07-25 19:21:56 -0700

    cefcontainment: tabs to spaces

--- cefcontainment.cs
+++ cefcontainment.cs
@@ -8,32 +8,29 @@ using MCGalaxy.Events.LevelEvents;
 using MCGalaxy.Events.PlayerEvents;
 
 namespace MCGalaxy {
-	
-	public sealed class PluginCefContainment : Plugin {
-		public override string name { get { return "CefContainment"; } }
-		public override string MCGalaxy_Version { get { return "1.9.0.5"; } }
-		public override string creator { get { return "Goodly"; } }
-		
-		public override void Load(bool startup) {
-			OnPlayerChatEvent.Register(RightBeforeChat, Priority.High);
-		}
-		
-		public override void Unload(bool shutdown) {
-			OnPlayerChatEvent.Unregister(RightBeforeChat);
-		}
-		
-		static void RightBeforeChat(Player p, string message) {
-			
-			if (Colors.Strip(message).StartsWith("cef ")) {
-				p.cancelchat = true;
-				if (!LevelInfo.Check(p, p.Rank, p.level, "use cef commands in this level.")) return;
-				Chat.MessageFromLevel(p, "<Local>λFULL: &f" +message);
-				return;
-			}
+    
+    public sealed class PluginCefContainment : Plugin {
+        public override string name { get { return "CefContainment"; } }
+        public override string MCGalaxy_Version { get { return "1.9.0.5"; } }
+        public override string creator { get { return "Goodly"; } }
+        
+        public override void Load(bool startup) {
+            OnPlayerChatEvent.Register(RightBeforeChat, Priority.High);
+        }
+        
+        public override void Unload(bool shutdown) {
+            OnPlayerChatEvent.Unregister(RightBeforeChat);
+        }
+        
+        static void RightBeforeChat(Player p, string message) {
+            
+            if (Colors.Strip(message).StartsWith("cef ")) {
+                p.cancelchat = true;
+                if (!LevelInfo.Check(p, p.Rank, p.level, "use cef commands in this level.")) return;
+                Chat.MessageFromLevel(p, "<Local>λFULL: &f" +message);
+                return;
+            }
 
-		}
-		
-
-		
-	}
+        }
+    }
 }
